19080356d34e4934a51fb44243357bd7628342cf
MN <mnegretetapia1@toromai.csudh.edu>
2026-03-04 00:58:15 +0000

    lab7

---
+++ mnegrete7.py
@@ -0,0 +1,27 @@
+ #Moises Negrete
+ #lab #7
+
+
+
+x = 5
+
+if x > 6:
+    print("X is greater than 6")
+elif x < 6:
+    print("X is less than 6 and the value of x is", x)
+else:
+    print("error")
+
+z = 0.5
+
+if x < 2:
+    print(z, "is less than 2")
+else:
+    print(z, "is greater than 2")
+
+s = "cat"
+
+if s == "dog":
+    print("s is a dog")
+else:
+    print("s is not a dog")
